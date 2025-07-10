9d2d51bde3db23699d8d79a18403288276fa81f5
Kushal-Kamble <^[[200~professionalkamble199@gmail.com~>
2025-07-10 17:20:09 +0530

    react vite

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+import FunctionalComponentExample from './functionalComponentExample/FunctionalComponentExample'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-primary">Hello Bootstrap in Vite + React!</h1>
+
+        {/* FunctionalComponentExample */}
+        <FunctionalComponentExample />
+      
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
